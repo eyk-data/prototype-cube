6d230dbaca42c0d90c92b3a6c7f0d3eddb9f8d9a
Bart Lammers <bart.lammers@labela.nl>
2023-12-25 11:21:12 +0100

    Add second table to destination 2

--- dummy_data/fill_destination2.sql
+++ dummy_data/fill_destination2.sql
@@ -8,11 +8,25 @@ CREATE TABLE IF NOT EXISTS paid_performance (
 INSERT INTO paid_performance (source, campaign, impressions, clicks)
 VALUES
     ('facebook', 'Destination 2 - campaign 1', 5670, 53),
-    ('facebook', 'Destination 2 - campaign 2', 7246, 62),
     ('facebook', 'Destination 2 - campaign 3', 1002, 770),
     ('linkedin', 'Destination 2 - other campaign 1', 41450, 210),
     ('linkedin', 'Destination 2 - other campaign 2', 6250, 114),
     ('google_ads', 'Destination 2 - google ads campaign 1', 14140, 537),
-    ('google_ads', 'Destination 2 - google ads campaign 2', 9679, 79),
-    ('google_ads', 'Destination 2 - google ads campaign 3', 9064, 135),
     ('google_ads', 'Destination 2 - google ads campaign 4', 46733, 477);
+
+CREATE TABLE IF NOT EXISTS ecommerce_attribution_models (
+  model varchar(250) NOT NULL,
+  source varchar(250) NOT NULL,
+  medium varchar(250) NOT NULL,
+  campaign varchar(250) NOT NULL,
+  revenue NUMERIC(10, 2) NOT NULL
+);
+
+INSERT INTO ecommerce_attribution_models (model, source, medium, campaign, revenue)
+VALUES
+    ('first_touch', 'google', 'cpc', 'Destination 2 campaign 1', 101.11),
+    ('linear', 'google', 'organic', '', 5.19),
+    ('last_touch', 'facebook', 'social', '', 408.93),
+    ('linear', 'google', 'organic', '', 15.01),
+    ('first_touch', 'google', 'cpc', 'Destination 2 campaign 2', 123.00),
+    ('time_decay', 'direct', '', '', 99.13);
