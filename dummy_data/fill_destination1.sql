87b44af665cb8facfb5c7d4d1b193253a257b8fb
Bart Lammers <bart.lammers@labela.nl>
2023-12-26 17:50:45 +0100

    Update data, add ecommerce attribution data model

--- dummy_data/fill_destination1.sql
+++ dummy_data/fill_destination1.sql
@@ -8,11 +8,6 @@ CREATE TABLE IF NOT EXISTS paid_performance (
 INSERT INTO paid_performance (source, campaign, impressions, clicks)
 VALUES
     ('facebook', 'Destination 1 - campaign 1', 130, 16),
-    ('facebook', 'Destination 1 - campaign 2', 10, 8),
-    ('facebook', 'Destination 1 - campaign 3', 340, 100),
     ('linkedin', 'Destination 1 - other campaign 1', 770, 70),
     ('linkedin', 'Destination 1 - other campaign 2', 1080, 320),
-    ('google_ads', 'Destination 1 - google ads campaign 1', 6750, 526),
-    ('google_ads', 'Destination 1 - google ads campaign 2', 8654, 352),
-    ('google_ads', 'Destination 1 - google ads campaign 3', 343, 74),
-    ('google_ads', 'Destination 1 - google ads campaign 4', 577, 73);
+    ('google_ads', 'Destination 1 - google ads campaign 3', 343, 74);
--- dummy_data/fill_destination2.sql
+++ dummy_data/fill_destination2.sql
@@ -8,10 +8,8 @@ CREATE TABLE IF NOT EXISTS paid_performance (
 INSERT INTO paid_performance (source, campaign, impressions, clicks)
 VALUES
     ('facebook', 'Destination 2 - campaign 1', 5670, 53),
-    ('facebook', 'Destination 2 - campaign 3', 1002, 770),
-    ('linkedin', 'Destination 2 - other campaign 1', 41450, 210),
     ('linkedin', 'Destination 2 - other campaign 2', 6250, 114),
-    ('google_ads', 'Destination 2 - google ads campaign 1', 14140, 537),
+    ('google_ads', 'Destination 2 - google ads campaign 3', 14140, 537),
     ('google_ads', 'Destination 2 - google ads campaign 4', 46733, 477);
 
 CREATE TABLE IF NOT EXISTS ecommerce_attribution_models (
@@ -27,6 +25,5 @@ VALUES
     ('first_touch', 'google', 'cpc', 'Destination 2 campaign 1', 101.11),
     ('linear', 'google', 'organic', '', 5.19),
     ('last_touch', 'facebook', 'social', '', 408.93),
-    ('linear', 'google', 'organic', '', 15.01),
     ('first_touch', 'google', 'cpc', 'Destination 2 campaign 2', 123.00),
     ('time_decay', 'direct', '', '', 99.13);
